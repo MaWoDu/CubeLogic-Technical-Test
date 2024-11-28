96e60050fbfcae13949143a7a65c1539b705af69
mawodu <durkiewiczmaciej@gmail.com>
2024-11-28 12:16:13 +0000

    chore(project): init java17+gradle boilerplate

---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "cubelogic-technical-interview"
+
---
+++ src/main/java/org/cubelogic/Main.java
@@ -0,0 +1,7 @@
+package org.cubelogic;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
